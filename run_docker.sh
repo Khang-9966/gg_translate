7b07210c4a150dc2d89b81806e7b458093815e42
khang-9966 <dtkhangbk@gmail.com>
2023-12-25 19:16:14 +0700

    edit port

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
-sudo docker run --name gg_trans_1 -p 0.0.0.0:1234:80 -p 0.0.0.0:1235:5900 -p 0.0.0.0:1236:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api
+sudo docker run --name gg_trans_1 -p 0.0.0.0:3234:80 -p 0.0.0.0:3235:5900 -p 0.0.0.0:3236:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api
 
--- run_many_dockers.sh
+++ run_many_dockers.sh
@@ -1,4 +1,4 @@
-docker run --name gg_trans_1 -p 0.0.0.0:1234:80 -p 0.0.0.0:1235:5900 -p 0.0.0.0:1236:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
-docker run --name gg_trans_3 -p 0.0.0.0:1334:80 -p 0.0.0.0:1335:5900 -p 0.0.0.0:1336:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
-docker run --name gg_trans_4 -p 0.0.0.0:1434:80 -p 0.0.0.0:1435:5900 -p 0.0.0.0:1436:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
-docker run --name gg_trans_5 -p 0.0.0.0:1534:80 -p 0.0.0.0:1535:5900 -p 0.0.0.0:1536:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api
+docker run --name gg_trans_1 -p 0.0.0.0:3234:80 -p 0.0.0.0:3235:5900 -p 0.0.0.0:3236:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
+docker run --name gg_trans_3 -p 0.0.0.0:3334:80 -p 0.0.0.0:3335:5900 -p 0.0.0.0:3336:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
+docker run --name gg_trans_4 -p 0.0.0.0:3434:80 -p 0.0.0.0:3435:5900 -p 0.0.0.0:3436:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api & \
+docker run --name gg_trans_5 -p 0.0.0.0:3534:80 -p 0.0.0.0:3535:5900 -p 0.0.0.0:3536:8889 -e VNC_PASSWORD=123456 -e RESOLUTION=1280x720 --shm-size 16g  gg_translate_api
